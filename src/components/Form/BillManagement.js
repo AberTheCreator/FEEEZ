ce8770ccc43890b093681c698f5515f513a3dda1
AberTheCreator <PaulAber68@gmail.com>
2025-09-09 23:28:38 +0100

    Create BillManagement.js

---
+++ src/components/Form/BillManagement.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import BillForm from './BillForm';
+import PaymentForm from './PaymentForm';
+
+
+const BillManagement = () => {
+  const [showBillForm, setShowBillForm] = useState(true);
+  const [selectedBill, setSelectedBill] = useState(null);
+
+  const handleBillFormSubmit = data => {
+    
+    setShowBillForm(false);
+    setSelectedBill(data);
+  };
+
+  const handlePaymentFormSubmit = data => {
+    
+    setShowBillForm(true);
+    setSelectedBill(null);
+  };
+
+  return (
+    <div>
+      {showBillForm ? (
+        <BillForm
+          onSubmit={handleBillFormSubmit}
+          onCancel={() => setShowBillForm(false)}
+        />
+      ) : (
+        <PaymentForm
+          bill={selectedBill}
+          onSubmit={handlePaymentFormSubmit}
+          onCancel={() => setShowBillForm(true)}
+        />
+      )}
+    </div>
+  );
+};
+
+export default BillManagement;
